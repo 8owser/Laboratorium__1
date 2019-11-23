d026e86609edef4c0a3c5c6e4e6299554c462ea3
8owser <patrykstark@gmail.com>
2019-11-23 16:32:03 +0100

    adding project for the uni thing

---
+++ app/src/main/java/com/example/studentwsb/laboratorium__1/AbsenceActivity.java
@@ -0,0 +1,13 @@
+package com.example.studentwsb.laboratorium__1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AbsenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absence);
+    }
+}
---
+++ app/src/main/java/com/example/studentwsb/laboratorium__1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.studentwsb.laboratorium__1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+                Intent intent = new Intent(this, AbsenceActivity.class);
+                startActivity(intent);
+        });
+
+
+    }
+}
